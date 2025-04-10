2ce18c9ac163f52e201518224d4334aed142e46f
gabri-beep <biel790832157@gmail.com>
2025-04-10 14:07:29 -0300

    commit_inicial

---
+++ Biblioteca_Jpa/Biblioteca_Jpa/src/main/java/com/example/Biblioteca_Jpa/BibliotecaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaJpaApplication.class, args);
+	}
+
+}
---
+++ Biblioteca_Jpa/Biblioteca_Jpa/src/test/java/com/example/Biblioteca_Jpa/BibliotecaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
